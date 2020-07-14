b4ec7413c7f5540b5fbbe7ebc8a024af7977f4dd
GarhettM <runner@fv-az21.lc5011gtqsde5hsipp0qfbezsh.bx.internal.cloudapp.net>
2020-07-14 23:41:16 +0000

    Updates

---
+++ precache-manifest.ae115418c7d7abef23365048e0abf361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3559853529b215e61f743e12ab7c4f5c",
+    "url": "/RESTy/index.html"
+  },
+  {
+    "revision": "18c65a17d638bfb207ac",
+    "url": "/RESTy/static/css/main.218cf8ff.chunk.css"
+  },
+  {
+    "revision": "ba25565b63430d2febae",
+    "url": "/RESTy/static/js/2.66cb5cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTy/static/js/2.66cb5cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c65a17d638bfb207ac",
+    "url": "/RESTy/static/js/main.2958e3b6.chunk.js"
+  },
+  {
+    "revision": "86ce915c702c61eb4d05",
+    "url": "/RESTy/static/js/runtime-main.cc6e0ddb.js"
+  }
+]);
